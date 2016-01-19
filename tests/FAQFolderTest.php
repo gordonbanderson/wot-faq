7ae8cda3efaa538c3acbbb6f3121305b496a1cd2
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-19 22:49:55 +0700

    WIP: Initial tests shell

---
+++ tests/FAQFolderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class FAQFolderTest extends SapphireTest {
+	public function testGetLumberjackTitle() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testGetCMSFields() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
